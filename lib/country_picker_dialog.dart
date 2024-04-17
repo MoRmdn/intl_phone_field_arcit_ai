ecb653292e26b85d44404ee156476c757c5e95c6
Mohamed Ramadan <60930725+MoRmdn@users.noreply.github.com>
2024-04-17 15:34:02 +0200

    update with safe area

--- lib/country_picker_dialog.dart
+++ lib/country_picker_dialog.dart
@@ -111,126 +111,129 @@ class _CountryPickerDialogState extends State<CountryPickerDialog> {
     // final width = widget.style?.width ?? mediaWidth;
     // const defaultHorizontalPadding = 40.0;
     // const defaultVerticalPadding = 24.0;
-    return Container(
-      decoration: const BoxDecoration(
-        color: Color(0xfffefeff),
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(25),
-          topRight: Radius.circular(25),
+    return SafeArea(
+      top: false,
+      child: Container(
+        decoration: const BoxDecoration(
+          color: Color(0xfffefeff),
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(25),
+            topRight: Radius.circular(25),
+          ),
         ),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          //* create center container with grey background
-          Container(
-            alignment: Alignment.center,
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(25),
-                topRight: Radius.circular(25),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //* create center container with grey background
+            Container(
+              alignment: Alignment.center,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(25),
+                  topRight: Radius.circular(25),
+                ),
+              ),
+              height: widget.headerHeight ?? 60,
+              child: widget.header,
+            ),
+            const Divider(
+              thickness: 1,
+              color: Color(0xffe9e8e8),
+            ),
+
+            widget.subTitle,
+
+            Expanded(
+              child: ListView.builder(
+                shrinkWrap: true,
+                itemCount: _filteredCountries.length,
+                itemBuilder: (ctx, index) => Padding(
+                  padding: const EdgeInsets.only(
+                    left: 15,
+                    right: 10,
+                  ),
+                  child: Column(
+                    children: <Widget>[
+                      ListTile(
+                        leading: kIsWeb
+                            ? Image.asset(
+                                'assets/flags/${_filteredCountries[index].code.toLowerCase()}.png',
+                                package: 'intl_phone_field',
+                                width: 32,
+                              )
+                            : Text(
+                                _filteredCountries[index].flag,
+                                style: const TextStyle(fontSize: 18),
+                              ),
+                        contentPadding: const EdgeInsets.symmetric(horizontal: 5, vertical: 0),
+                        title: Text(
+                          _filteredCountries[index].localizedName(widget.languageCode),
+                          style: widget.countryNameStyle,
+                        ),
+                        trailing: Radio<Country>.adaptive(
+                          activeColor: widget.radioActiveColor,
+                          focusColor: widget.radioFocusColor,
+                          hoverColor: widget.radioHoverColor,
+                          overlayColor: widget.radioOverlayColor,
+                          fillColor: widget.radioFillColor,
+                          value: _filteredCountries[index],
+                          groupValue: _selectedCountry,
+                          onChanged: (value) {
+                            _selectedCountry = value!;
+                            setState(() {});
+                          },
+                        ),
+                        onTap: () => setState(() {
+                          _selectedCountry = _filteredCountries[index];
+                        }),
+                      ),
+
+                      // widget.style?.listTileDivider ?? const Divider(thickness: 1),
+                    ],
+                  ),
+                ),
               ),
             ),
-            height: widget.headerHeight ?? 60,
-            child: widget.header,
-          ),
-          const Divider(
-            thickness: 1,
-            color: Color(0xffe9e8e8),
-          ),
-
-          widget.subTitle,
-
-          Expanded(
-            child: ListView.builder(
-              shrinkWrap: true,
-              itemCount: _filteredCountries.length,
-              itemBuilder: (ctx, index) => Padding(
-                padding: const EdgeInsets.only(
-                  left: 15,
-                  right: 10,
+            Container(
+                width: mediaWidth,
+                height: widget.bottomHight ?? 90,
+                padding: widget.padding,
+                decoration: BoxDecoration(
+                  borderRadius: const BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                  border: Border.all(
+                    color: const Color(0xffe9e8e8),
+                    width: 1,
+                  ),
                 ),
-                child: Column(
-                  children: <Widget>[
-                    ListTile(
-                      leading: kIsWeb
-                          ? Image.asset(
-                              'assets/flags/${_filteredCountries[index].code.toLowerCase()}.png',
-                              package: 'intl_phone_field',
-                              width: 32,
-                            )
-                          : Text(
-                              _filteredCountries[index].flag,
-                              style: const TextStyle(fontSize: 18),
-                            ),
-                      contentPadding: const EdgeInsets.symmetric(horizontal: 5, vertical: 0),
-                      title: Text(
-                        _filteredCountries[index].localizedName(widget.languageCode),
-                        style: widget.countryNameStyle,
-                      ),
-                      trailing: Radio<Country>.adaptive(
-                        activeColor: widget.radioActiveColor,
-                        focusColor: widget.radioFocusColor,
-                        hoverColor: widget.radioHoverColor,
-                        overlayColor: widget.radioOverlayColor,
-                        fillColor: widget.radioFillColor,
-                        value: _filteredCountries[index],
-                        groupValue: _selectedCountry,
-                        onChanged: (value) {
-                          _selectedCountry = value!;
-                          setState(() {});
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: GestureDetector(
+                        onTap: () {
+                          Navigator.pop(context);
                         },
+                        child: widget.cancelButton,
                       ),
-                      onTap: () => setState(() {
-                        _selectedCountry = _filteredCountries[index];
-                      }),
                     ),
-
-                    // widget.style?.listTileDivider ?? const Divider(thickness: 1),
+                    Expanded(
+                      child: GestureDetector(
+                        onTap: () {
+                          widget.onCountryChanged(_selectedCountry);
+                          Navigator.pop(context);
+                        },
+                        child: widget.doneButton,
+                      ),
+                    ),
                   ],
+                )
+                // color: Colors.grey[300],
                 ),
-              ),
-            ),
-          ),
-          Container(
-              width: mediaWidth,
-              height: widget.bottomHight ?? 90,
-              padding: widget.padding,
-              decoration: BoxDecoration(
-                borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(20),
-                  topRight: Radius.circular(20),
-                ),
-                border: Border.all(
-                  color: const Color(0xffe9e8e8),
-                  width: 1,
-                ),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Expanded(
-                    child: GestureDetector(
-                      onTap: () {
-                        Navigator.pop(context);
-                      },
-                      child: widget.cancelButton,
-                    ),
-                  ),
-                  Expanded(
-                    child: GestureDetector(
-                      onTap: () {
-                        widget.onCountryChanged(_selectedCountry);
-                        Navigator.pop(context);
-                      },
-                      child: widget.doneButton,
-                    ),
-                  ),
-                ],
-              )
-              // color: Colors.grey[300],
-              ),
-        ],
+          ],
+        ),
       ),
     );
   }
